Fixed dimensionality issue with KL

experiments/toy_regression.py
@@ -2,20 +2,26 @@ from __future__ import annotations
 
 import os
 import sys
-
+from datetime import datetime
 from typing import Callable
 
 file_dir = os.path.dirname(__file__)
 sys.path.insert(0, os.path.abspath(os.path.join(file_dir, "..")))
+
+import logging
+from pathlib import Path
 
 import gi
 import lab as B
 import lab.torch
 import torch
 import torch.nn as nn
+from gi.utils.plotting import plot
 
-from varz import Vars
-import logging
+import argparse 
+
+from varz import Vars, namespace
+import numpy as np
 
 logger = logging.getLogger(__name__)
 
@@ -80,6 +86,8 @@ def build_z(key: B.RandomState, M: B.Int, x, y):
 
     :param zs: inducing inputs
     :param yz: pseudo (inducing) outputs for final layer
+
+    :returns: key, z, y
     """
     if M <= len(x):
         key, idx = B.randperm(key, B.default_dtype, M)
@@ -124,7 +132,7 @@ def estimate_elbo(key: B.RandomState, model: gi.GIBNN, likelihood: Callable,
             zs: dict[str, B.Numeric], S, N):
     
     key, _cache = model.sample_posterior(key, ps, ts, zs, S)
-    out = model.propagate(x)
+    out = model.propagate(x) # out : [S x N x Dout]
     
     # Compute KL divergence.
     kl = 0.
@@ -134,19 +142,92 @@ def estimate_elbo(key: B.RandomState, model: gi.GIBNN, likelihood: Callable,
     # Compute the expected log-likelihood.
     exp_ll = likelihood(out).logpdf(y)
     exp_ll = exp_ll.mean(0).sum() # take mean across inference samples and sum
+    kl = kl.mean() # across inference samples
     
-    logger.debug(f"ll: {exp_ll}")
-
     # Mini-batching estimator of ELBO
-    return key, ((N / len(x)) * exp_ll) - kl
+    elbo = ((N / len(x)) * exp_ll) - kl
+    
+    logger.debug(f"elbo: {round(elbo.item(), 0):10.1f}, ll: {round(exp_ll.item(), 0):10.1f}, kl: {round(kl.item(), 1):10.1f}")
 
+    return key, elbo
+
+@namespace("zs")
+def add_zs(vs, zs):
+    """ Add client inducing points to optimizable params in vs """
+    # TODO: for some reason, the (client-local) inducing points do not change across epochs
+    for client_name, client_z in zs.items():
+        vs.unbounded(client_z, name=f"{client_name}_z")
+
+@namespace("ts")
+def add_ts(vs, ts):
+    """ Add client likelihood factors to optimizable params in vs """
+    for client_name, client_dict in ts.items():
+        for layer_name, t in client_dict.items():
+            vs.unbounded(t.yz, name=f"{client_name}_{layer_name}_yz")
+            vs.positive(t.nz, name=f"{client_name}_{layer_name}_nz")
+
+def track_change(opt, vs, var_names, i, epoch, olds):
+    """ Steps optimizer and reports delta for variables when iteration%epoch == 0"""
+    _olds = olds
+
+    opt.step()
+
+    if i%epoch==0:
+        # Report deltas
+        for var_name in var_names:
+            if _olds != {}:
+                _delta = vs[var_name].detach().cpu().squeeze() - _olds[var_name]
+            else:
+                _delta = vs[var_name].detach().cpu().squeeze()
+
+            logger.debug(f"Δ{var_name}: {np.array(_delta)}")
+
+        # Update olds
+        for var_name in var_names:
+            _old = vs[var_name].detach().cpu().squeeze()
+            _olds[var_name] = _old
+
+    return opt, olds
+
+def track(opt, vs, var_names, i, epoch):
+    opt.step()
+
+    for var_name in var_names:
+        _v = vs[var_name].detach().cpu().squeeze()
+        if i%epoch == 0: logger.debug(f"{var_name}: {np.array(_v)}")
+
+    return opt
 
 if __name__ == "__main__":
-    logger.setLevel(logging.INFO)
-
-    B.default_dtype = torch.float64
-    key = B.create_random_state(B.default_dtype, seed=0)
+    parser = argparse.ArgumentParser()
     
+    parser.add_argument('--verbose', '-v', action='store_true', help='Sets the log level to DEBUG')
+    parser.add_argument('--seed', type=int, help='seed', nargs='?', default=0)
+    parser.add_argument('--epochs', '-e', type=int, help='epochs', default=1000)
+    parser.add_argument('--plot', '-p', type=bool, action='store_true', help='Plot results')
+    # parser.add_argument('--task', choices=['reg', 'class'], help='Task')
+    args = parser.parse_args()
+
+    # Logging settings
+    if args.verbose: 
+        logging.getLogger().setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+    np.set_printoptions(linewidth=np.inf)
+    start_time = datetime.utcnow().strftime("%Y-%m-%d-%H.%M.%S")
+    _start = datetime.utcnow().strftime("%Y-%m-%d-%H.%M")
+    
+    # Create directory for saving plots
+    file_dir = os.path.dirname(__file__)
+    save_dir = os.path.join(file_dir, "../plots/")
+    fdir = os.path.join(save_dir, _start)
+    Path(fdir).mkdir(parents=True, exist_ok=True)
+
+    # Lab variable initialization
+    B.default_dtype = torch.float64
+    key = B.create_random_state(B.default_dtype, seed=args.seed)
+    
+    # Generate regression data
     key, x_tr, y_tr = generate_data(key, size=100)
     key, x_te, y_te = generate_test_data(key, size=50)
     
@@ -154,11 +235,11 @@ if __name__ == "__main__":
     model = gi.GIBNN(nn.functional.relu)
 
     # Build one client
-    M = 10 # number of inducing points
+    M = 100 # number of inducing points
     ps = build_prior(1, 50, 50, 1)
     key, z, yz = build_z(key, M, x_tr, y_tr)
     t = build_ts(key, M, yz, 1, 50, 50, 1, nz_init=1e-3)
-    
+
     # Collect clients
     ts = {k: {"client0": v} for k, v in t.items()} # flip order bc ts = dict<k=layer, v=pseudo obs>
     zs = {"client0": z}
@@ -170,25 +251,29 @@ if __name__ == "__main__":
     output_var = vs.positive(1e-3, name="output_var")
     likelihood = gi.likelihoods.NormalLikelihood(output_var)
     
-    for client_name, client_z in zs.items():
-        vs.unbounded(client_z, name=f"{client_name}_z")
+    # Add zs, ts to optimizable containers
+    add_zs(vs, zs)
+    add_ts(vs, ts)
     
-    for client_name, client_dict in ts.items():
-        for layer_name, t in client_dict.items():
-            vs.unbounded(t.yz, name=f"{client_name}_{layer_name}_yz")
-            vs.positive(t.nz, name=f"{client_name}_{layer_name}_nz")
-
     # Set requirement for gradients    
     vs.requires_grad(True, *vs.names) # By default, no variable requires a gradient in Varz
     
-    opt = torch.optim.Adam(vs.get_vars(), lr=1e-3)
+    # Optimizer parameters
+    opt = torch.optim.Adam(vs.get_vars(), lr=1e-2)
     N = len(x_tr)
     batch_size = min(100, N)
-    S = 1 # number of inference samples
+    S = 2 # number of inference samples
     mb_idx = 0 # minibatch idx
-    for i in range(1000):
+    epochs = args.epochs
+    
+    
+    # Logging
+    log_step = 100 # report change in param values every <log_step> epochs
+    olds = {} # to keep track of old parameter values
+    
+    for i in range(epochs):
         
-        # Construct i-th {x, y} training batch batch 
+        # Construct i-th minibatch {x, y} training data
         inds = B.range(batch_size) + mb_idx
         x_mb = B.take(x_tr, inds) # take() is for JAX
         y_mb = B.take(y_tr, inds)
@@ -198,8 +283,14 @@ if __name__ == "__main__":
         loss = -elbo
         loss.backward()
 
-        if i%100 == 0:
-            logger.info(f"Epoch {i} - loss: {int(loss.item())}")
+        if i%100 == 0: 
+            logger.info(f"Epoch {i} - loss (elbo): {int(loss.item())}")
 
-        opt.step()
+            if args.plot: 
+                plot(fdir=fdir, fname=f"{start_time}_{i}", 
+                    x1=zs["client0"].mean(0), y1=ts[list(ts.keys())[-1]]["client0"].yz, x2=x_mb, y2=y_mb, 
+                    desc1="Variational params", desc2="Training data")
+        
+        opt, olds = track_change(opt, vs, ['ts.layer2_client0_yz', 'ts.layer0_client0_nz'], i, 100, olds)
         opt.zero_grad()
+