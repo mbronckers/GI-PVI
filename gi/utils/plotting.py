cb3286d1e75a880f64b742619d2557617d44ff54
Max Bronckers <maxbronckers@me.com>
2022-05-31 18:10:34 +0100

    File cleanup

--- gi/utils/plotting.py
+++ gi/utils/plotting.py
@@ -40,30 +40,6 @@ def scatter_plot(fdir, fname, x1: Tensor, y1: Tensor, x2: Tensor, y2: Tensor,
 
     plt.savefig(os.path.join(fdir, f'{fname}.png'), pad_inches=0.2, bbox_inches='tight')
 
-# def scatter_plot(fdir, fname, x1: Tensor, y1: Tensor, desc1: str, xlabel: Optional[str]=None, ylabel: Optional[str]=None, title: Optional[str]=None):
-
-#     assert os.path.exists(fdir)
-#     logger.info(f"Saving plot in {fdir}")
-
-#     fig, ax = plt.subplots(1, 1, figsize=(10,10))
-#     lw = 2 # linewidth=lw
-
-#     def get_values(x):
-#         return np.array(x.squeeze().detach().cpu())
-
-#     sns.scatterplot(y=get_values(y1), x=get_values(x1), label=f"{desc1}", ax=ax, 
-#                 color=colors[0])
-
-#     ax.legend()
-#     # ax.set_xlabel(f'{}')
-#     # ax.set(ylim=(0.60, 1.01), xlim=(-0.005, 0.20))
-#     if xlabel != None: ax.set_xlabel(xlabel)
-#     if ylabel != None: ax.set_ylabel(ylabel)
-#     if title != None: ax.set_title(title)
-#     ax.legend(loc='upper right', prop={'size': 12})
-
-#     plt.savefig(os.path.join(fdir, f'{fname}.png'), pad_inches=0.2, bbox_inches='tight')
-
 def line_plot(fdir, fname, x, y, desc, xlabel=None, ylabel=None, title=None):
     assert os.path.exists(fdir)
     logger.info(f"Saving plot in {fdir}")
