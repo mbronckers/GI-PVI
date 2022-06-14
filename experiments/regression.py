c5398252362baa75f7825937548e5643833f1f30
Max Bronckers <maxbronckers@me.com>
2022-06-14 15:16:45 +0100

    Adding sampled functions plots

--- experiments/regression.py
+++ experiments/regression.py
@@ -28,7 +28,7 @@ import torch.nn as nn
 import seaborn as sns
 from varz import Vars, namespace
 from wbml import experiment, out, plot
-from gi.utils.plotting import line_plot, plot_confidence, scatter_plot
+from gi.utils.plotting import line_plot, plot_confidence, scatter_plot, plot_predictions
 
 from priors import build_prior, parse_prior_arg
 from dgp import generate_data, split_data, DGP
@@ -143,17 +143,25 @@ def eval_logging(x, y, x_tr, y_tr, y_pred, error, pred_var, data_name, _results_
 
     # Plot model predictions
     if _plot:
+    
+        # Plot eval data, training data, and model predictions (in that order)
         _ax = scatter_plot(x, y, x_tr, y_tr, f"{data_name.capitalize()} data", "Training data", "x", "y", f"Model predictions on {data_name.lower()} data ({_S} samples)")
         scatterplot = plot.patch(sns.scatterplot)
         scatterplot(ax=_ax, y=y_pred.mean(0), x=x, label="Model predictions", color=gi.utils.plotting.colors[3])
         _ax.legend()
 
+        # Plot confidence bounds
         _preds_idx = [f'preds_{i}' for i in range(_S)]
         quartiles = np.quantile(_results_eval[_preds_idx], np.array((0.05,0.25,0.75,0.95)), axis=1) # [num quartiles x num preds]
         _ax = plot_confidence(_ax, x.squeeze().detach().cpu(), quartiles, all=True)
-        
-
         plt.savefig(os.path.join(_plot_dir, f'{_fname}.png'), pad_inches=0.2, bbox_inches='tight')
+
+        # Plot all sampled functions
+        ax = plot_predictions(None, x, y_pred, "Model predictions", "x", "y", f"Model predictions on {data_name.lower()} data ({_S} samples)")
+        
+        _sampled_funcs_dir = os.path.join(_plot_dir, "sampled_funcs")
+        Path(_sampled_funcs_dir).mkdir(parents=True, exist_ok=True)
+        plt.savefig(os.path.join(_sampled_funcs_dir, f'{_fname}_samples.png'), pad_inches=0.2, bbox_inches='tight')
 
 
 if __name__ == "__main__":
