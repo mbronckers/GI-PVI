3a588199554d00f90d160894832e2704c0193890
Max Bronckers <maxbronckers@me.com>
2022-06-24 17:10:50 +0200

    Adding temporary PVI results for inspection

---
+++ results/2022-06-24-15.04.28_current_pvi/config.py
@@ -0,0 +1,99 @@
+from __future__ import annotations
+
+import sys
+import os
+from dataclasses import dataclass
+
+file_dir = os.path.dirname(__file__)
+_root_dir = os.path.abspath(os.path.join(file_dir, ".."))
+sys.path.insert(0, os.path.abspath(_root_dir))
+
+from priors import Prior
+from dgp import DGP
+import lab as B
+import torch
+
+@dataclass
+class Config:
+    name: str = ""
+    seed: int = 0
+    plot: bool = True
+    
+    iters: int = 100    # server iterations 
+    epochs: int = 100   # client epochs
+    
+    N: int = 100        # Number of training data pts
+    M: int = 10         # Number of inducing points
+    S: int = 10         # Number of training weight samples
+    I: int = 100        # Number of inference samples
+
+    batch_size: int = 100
+
+    nz_init: float = B.exp(-4)  # precision
+    ll_var: float = 1e-3        # likelihood variance
+
+    # Learning rates
+    separate_lr: bool = False       # use seperate learning rates
+    lr_global: float = 1e-2
+    lr_nz: float = 1e-3
+    lr_output_var: float = 1e-3
+    lr_client_z: float = lr_global
+    lr_yz: float = lr_global
+    
+    prior: Prior = Prior.StandardPrior
+    dgp: DGP = DGP.ober_regression
+    optimizer: str = "Adam"
+
+    random_z: bool = False
+    bias: bool = True
+
+    dims = [1, 50, 50, 1]
+
+    load: str = None
+
+    start = None
+    start_time = None
+    results_dir = None
+    wd = None
+    plot_dir = None
+    metrics_dir = None
+    model_dir = None
+    training_plot_dir = None
+
+    # Clients
+    num_clients: int = 1
+    def __post_init__(self):
+        self.client_splits: list[float] = [1.]
+        self.optimizer_params: dict = {"lr": self.lr_global}
+    
+################################################################
+
+# The default config settings follow Ober et al.'s toy regression experiment details
+
+@dataclass
+class PVIConfig(Config):
+    name: str = "pvi"
+
+    epochs: int = 100
+    iters: int = 10
+
+    ll_var: float = 1e-2
+
+    num_clients: int = 2
+
+    def __post_init__(self):
+        self.client_splits: list[float] = [0.5, 0.5]
+        self.optimizer_params: dict = {"lr": self.lr_global}
+
+class Color:
+   PURPLE = '\033[95m'
+   CYAN = '\033[96m'
+   DARKCYAN = '\033[36m'
+   BLUE = '\033[94m'
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   WHITE = '\033[97m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
