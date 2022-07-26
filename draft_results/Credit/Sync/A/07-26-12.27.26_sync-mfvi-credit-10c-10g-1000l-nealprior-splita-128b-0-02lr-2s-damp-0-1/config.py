287d88856175d0c022c3e22566f7c9612caf250e
Max Bronckers <maxbronckers@me.com>
2022-07-26 13:57:02 +0100

    Dampened results.

---
+++ draft_results/Credit/Sync/A/07-26-12.27.26_sync-mfvi-credit-10c-10g-1000l-nealprior-splita-128b-0-02lr-2s-damp-0-1/config.py
@@ -0,0 +1,118 @@
+from __future__ import annotations
+
+import sys
+import os
+from dataclasses import dataclass
+
+file_dir = os.path.dirname(__file__)
+_root_dir = os.path.abspath(os.path.join(file_dir, ".."))
+sys.path.insert(0, os.path.abspath(_root_dir))
+
+from priors import Prior
+from dgp import DGP
+import lab as B
+import torch
+from gi.server import SequentialServer, Server, SynchronousServer
+from gi.mfvi import MFVI_Classification
+from gi.gibnn import GIBNN_Classification
+
+from .config import Config, set_experiment_name, set_partition_factors
+
+
+@dataclass
+class GI_CreditConfig(Config):
+    posterior_type: str = "pvi_credit"
+    location = os.path.basename(__file__)
+    dgp: DGP = DGP.uci_credit
+    model_type = GIBNN_Classification
+
+    prior: Prior = Prior.NealPrior
+
+    # GI settings
+    deterministic: bool = False  # deterministic client training
+    random_z: bool = False  # random inducing point initialization
+    linspace_yz: bool = False  # True => use linspace(-1, 1) for yz initialization
+
+    # Model architecture
+    N: int = 0.8  # train_split
+    M: int = 100
+    S: int = 2
+    I: int = 50
+    dims = [46, 50, 50, 2]
+
+    batch_size: int = 128  # None => full batch
+
+    # PVI architecture - server & clients
+    server_type: Server = SynchronousServer
+    num_clients: int = 10
+    global_iters: int = 10  # shared/global server iterations
+    local_iters: int = 2000  # client-local iterations
+
+    # Learning rates
+    sep_lr: bool = False  # True => use seperate learning rates
+    lr_global: float = 0.02
+    lr_nz: float = 0.05  # CIFAR from Ober uses log_prec_lr 3 factor
+    lr_client_z: float = 0.01
+    lr_yz: float = 0.01
+
+    # Partition settings
+    split_type: str = "A"
+    dampening_factor = 1.0
+
+    def __post_init__(self):
+        self.name = set_experiment_name(self)
+        set_partition_factors(self)
+
+        # Homogeneous, equal-sized split.
+        self.optimizer_params: dict = {"lr": self.lr_global * self.dampening_factor}
+
+        # Precisions of the inducing points per layer
+        self.nz_inits: list[float] = [1e3 - (self.dims[i] + 1) for i in range(len(self.dims) - 1)]
+
+
+@dataclass
+class MFVI_CreditConfig(Config):
+    posterior_type: str = "mfvi_credit"
+    location = os.path.basename(__file__)
+    dgp: DGP = DGP.uci_credit
+    model_type = MFVI_Classification
+
+    prior: Prior = Prior.NealPrior
+
+    # MFVI settings
+    deterministic: bool = False  # deterministic client training
+    random_mean_init: bool = False  # True => Initialize weight layer mean from N(0,1)
+
+    # Model architecture
+    N: int = 0.8  # train_split
+    S: int = 2
+    I: int = 50
+    dims = [46, 50, 50, 2]
+
+    batch_size: int = 128  # None => full batch
+
+    # PVI settings
+    server_type: Server = SynchronousServer
+    num_clients: int = 10
+    global_iters: int = 10  # shared/global server iterations
+    local_iters: int = 1000  # client-local iterations
+
+    # Learning rates
+    sep_lr: bool = False  # True => use seperate learning rates
+    lr_global: float = 0.02
+    lr_nz: float = 0.05
+    lr_yz: float = 0.01
+
+    # Partition settings
+    split_type: str = "A"
+    dampening_factor = 1e-1
+
+    def __post_init__(self):
+        self.name = set_experiment_name(self)
+        set_partition_factors(self)
+
+        # Homogeneous, equal-sized split.
+        self.optimizer_params: dict = {"lr": self.lr_global * self.dampening_factor}
+
+        # Precisions of the inducing points per layer
+        self.nz_inits: list[float] = [1e3 - (self.dims[i] + 1) for i in range(len(self.dims) - 1)]
